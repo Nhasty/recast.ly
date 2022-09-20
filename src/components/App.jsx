097111d0fd92b1910d05d5509af018b1e37b5066
nhasty <nyall.hasty@gmail.com>
2022-09-19 19:05:09 -0700

    VideoList, VideoListEntry, VideoPlayer, and App all working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,30 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import exampleVideoData from '../../../src/data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+var App = () => {
+  const [selectedVideo, setVideo] = React.useState(exampleVideoData[0]);
+  const selectVideo = (newVideo) => {
+    setVideo(newVideo);
+  };
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><VideoPlayer video={selectedVideo} /><h5><em>videoPlayer</em> view goes here</h5></div>
+        </div>
+        <div className="col-md-5">
+          <div><VideoList videos={exampleVideoData} selctVideo={selectVideo}/><h5><em>videoList</em> view goes here</h5></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+// debugger
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
